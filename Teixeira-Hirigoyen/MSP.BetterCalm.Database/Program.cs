3b5df2960409758517d6732da1a01e9e73b22b9b
Mauro Teixeira <MT211753@fi365.ort.edu.uy>
2021-04-10 12:06:21 -0300

    First commit

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.Database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSP.BetterCalm.Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MSP.BetterCalm.Domain
+{
+    public class Class1
+    {
+    }
+}
