74823cd31eb994154a2f87222a7b39271ff878ce
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-11 11:24:37 -0300

    NameEmpty Refactor

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.Domain/Playlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.Domain
+{
+    public class Playlist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool NameEmpty()
+        {
+            return this.Name.Length == 0;
+        }
+
+
+
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DomainTest/TestPlaylist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MSP.BetterCalm.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MSP.BetterCalm.DomainTest
+{
+    [TestClass]
+    public class TestPlaylist
+    {
+        Playlist playlist;
+        [TestInitialize]
+        public void Initialize()
+        {
+            playlist = new Playlist();
+      
+
+        }
+
+        [TestMethod]
+        public void NameEmpty()
+        {
+            playlist.Name = "";
+            Assert.IsTrue(playlist.NameEmpty());
+        }
+    }
+}
