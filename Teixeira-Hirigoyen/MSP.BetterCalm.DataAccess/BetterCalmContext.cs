4c9e7facb683072b017a58ac40ae5fe826192dcc
Mauro Teixeira <MT211753@fi365.ort.edu.uy>
2021-04-12 13:37:51 -0300

    Configuration ConnectionString and FirstMigration

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/BetterCalmContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MSP.BetterCalm.Domain;
+using System;
+using System.IO;
+
+namespace MSP.BetterCalm.DataAccess
+{
+    public class BetterCalmContext : DbContext
+    {
+        public virtual DbSet<Category> Category { get; set; }
+        public BetterCalmContext() { }
+        public BetterCalmContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string directory = Directory.GetCurrentDirectory();
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(directory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+                var connectionString = configuration.GetConnectionString(@"MSP.BetterCalmDB");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+    }
+}
