d6fb1a7a5a7640057e789366a1aafc8ae92b46ea
Mauro Teixeira <MT211753@fi365.ort.edu.uy>
2021-04-13 17:24:32 -0300

    RegisterCategoryTrack Refactor

--- Teixeira-Hirigoyen/MSP.BetterCalm.DomainTest/TestCategory.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DomainTest/TestCategory.cs
@@ -13,47 +13,58 @@ namespace MSP.BetterCalm.DomainTest
         Category category;
         Track track;
         Playlist playlist;
+        CategoryTrack categoryTrack;
+
         [TestInitialize]
         public void Initialize()
         {
-            category = new Category();
             playlist = new Playlist();
-            track = new Track();
-            
-            track.Id = 1;
-            track.Image = "";
-            track.Author = "";
-            track.Name = "Mauro baila cumbia";
-
             playlist.Id = 1;
             playlist.Name = "Musica para Mauro";
+            category = new Category
+            {
+                Name = "Dormir",
+                Id = 1,
+                Description = "Facil para dormir",
+                CategoryTrack = new List<CategoryTrack>(),
+               
+            };
+            track = new Track
+            {
+                Id = 1,
+                Image = "",
+                Author = "",
+                Name = "Mauro baila cumbia"
+                
+            };
+            categoryTrack = new CategoryTrack
+            {
+                IdCategory = category.Id,
+                Category = category,
+                IdTrack = track.Id,
+                Track = track,
+            };
+            category.CategoryTrack.Add(categoryTrack);
 
         }
 
         [TestMethod]
         public void RegisterName()
         {
-            category.Name = "Mauro";
-            Assert.AreEqual("Mauro", category.Name);
+            Assert.AreEqual("Dormir", category.Name);
         }
 
         [TestMethod]
         public void RegisterDescription()
         {
-            category.Description = "Facil para dormir";
             Assert.AreEqual("Facil para dormir", category.Description);
         }
-        /*[TestMethod]
-        public void RegisterTrack()
-        {
-            category.Track = track;
-            Assert.IsTrue(category.Track.Id == track.Id);
-        }
+
         [TestMethod]
-        public void RegisterPlayList()
+        public void RegisterCategoryTrack()
         {
-            category.Playlist = playlist;
-            Assert.IsTrue(category.Playlist.Id == playlist.Id);
-        }*/
+            Assert.IsTrue(category.CategoryTrack.Count == 1);
+        }
+        
     }
 }
