af59ecb89ee35978a35b4933e076a69283c81bb8
Mauro Teixeira <MT211753@fi365.ort.edu.uy>
2021-04-13 17:55:25 -0300

    SecondMigration and OnModelCreating

--- Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/BetterCalmContext.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/BetterCalmContext.cs
@@ -9,10 +9,28 @@ namespace MSP.BetterCalm.DataAccess
     public class BetterCalmContext : DbContext
     {
         public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Track> Track { get; set; }
+        public virtual DbSet<CategoryTrack> CategoryTrack { get; set; }
+
         public BetterCalmContext() { }
         public BetterCalmContext(DbContextOptions options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CategoryTrack>()
+                .HasKey(mc => new { mc.IdCategory, mc.IdTrack });
+            modelBuilder.Entity<CategoryTrack>()
+                .HasOne(mc => mc.Category)
+                .WithMany(m => m.CategoryTrack)
+                .HasForeignKey(mc => mc.IdCategory);
+            modelBuilder.Entity<CategoryTrack>()
+                .HasOne(mc => mc.Track)
+                .WithMany(c => c.CategoryTrack)
+                .HasForeignKey(mc => mc.IdTrack);
+        }
+
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
--- Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/Migrations/20210413205008_SecondMigration.Designer.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/Migrations/20210413205008_SecondMigration.Designer.cs
@@ -3,14 +3,16 @@ using MSP.BetterCalm.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MSP.BetterCalm.DataAccess.Migrations
 {
     [DbContext(typeof(BetterCalmContext))]
-    partial class BetterCalmContextModelSnapshot : ModelSnapshot
+    [Migration("20210413205008_SecondMigration")]
+    partial class SecondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
