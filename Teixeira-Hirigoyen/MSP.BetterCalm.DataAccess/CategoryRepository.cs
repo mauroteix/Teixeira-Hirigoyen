cc141cbca59eb974d4edfc33851aece6a1ca1d7e
Mauro Teixeira <MT211753@fi365.ort.edu.uy>
2021-04-15 17:49:01 -0300

    CategoryRepository Update

--- Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/CategoryRepository.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/CategoryRepository.cs
@@ -39,7 +39,8 @@ namespace MSP.BetterCalm.DataAccess
 
         public void Update(Category entity)
         {
-            throw new NotImplementedException();
+            _context.Category.Update(entity);
+            _context.SaveChanges();
         }
     }
 }
