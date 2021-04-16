0410090d0b43be99a84c014cb475e69b0a9f872f
Mauro Teixeira <MT211753@fi365.ort.edu.uy>
2021-04-16 18:47:28 -0300

    GetOneCategory Refactor

--- Teixeira-Hirigoyen/MSP.BetterCalm.DataAccesTest/CategoryRepositoryTest.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccesTest/CategoryRepositoryTest.cs
@@ -1,13 +1,48 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess;
+using MSP.BetterCalm.Domain;
+using System.Collections.Generic;
 
 namespace MSP.BetterCalm.DataAccessTest
 {
     [TestClass]
     public class CategoryRepositoryTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        List<Category> listCategories;
+        CategoryRepository repository;
+
+        [TestInitialize]
+        public void Initialize()
         {
+            listCategories = new List<Category>() {
+                new Category()
+                {
+                    Id = 1,
+                    Name = "Dormir",
+                    Description = "Musica para dormir tranquilo"
+                },
+                new Category()
+                {
+                    Id=2,
+                    Name="Meditar",
+                    Description = "Musica para relajarse"
+                }
+            };
+        }
+
+        [TestMethod]
+        public void GetOneCategory()
+        {
+            var options = new DbContextOptionsBuilder<BetterCalmContext>()
+                .UseInMemoryDatabase(databaseName: "MSP.BetterCalmDatabase").Options; 
+            var context = new BetterCalmContext(options);
+            listCategories.ForEach(cat => context.Add(cat));
+            context.SaveChanges();
+            repository = new CategoryRepository(context);
+            var category = repository.Get(listCategories[0].Id);
+            context.Database.EnsureDeleted();
+            Assert.AreEqual(listCategories[0].Id, category.Id);
         }
     }
 }
--- Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/BetterCalmContext.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/BetterCalmContext.cs
@@ -10,9 +10,12 @@ namespace MSP.BetterCalm.DataAccess
     {
         public virtual DbSet<Category> Category { get; set; }
         public virtual DbSet<Track> Track { get; set; }
+       // public virtual DbSet<Playlist> Playlist { get; set; }
         public virtual DbSet<CategoryTrack> CategoryTrack { get; set; }
+       /* public virtual DbSet<PlaylistCategory> PlaylistCategory { get; set; }
+        public virtual DbSet<PlaylistTrack> PlaylistTrack { get; set; }*/
 
-        public BetterCalmContext() { }
+    public BetterCalmContext() { }
         public BetterCalmContext(DbContextOptions options) : base(options)
         {
         }
@@ -27,6 +30,28 @@ namespace MSP.BetterCalm.DataAccess
             modelBuilder.Entity<CategoryTrack>()
                 .HasOne(mc => mc.Track)
                 .WithMany(c => c.CategoryTrack)
+                .HasForeignKey(mc => mc.IdTrack);
+
+            modelBuilder.Entity<PlaylistCategory>()
+               .HasKey(mc => new { mc.IdCategory, mc.IdPlaylist });
+            modelBuilder.Entity<PlaylistCategory>()
+                .HasOne(mc => mc.Category)
+                .WithMany(m => m.PlaylistCategory)
+                .HasForeignKey(mc => mc.IdCategory);
+            modelBuilder.Entity<PlaylistCategory>()
+                .HasOne(mc => mc.Playlist)
+                .WithMany(c => c.PlaylistCategory)
+                .HasForeignKey(mc => mc.IdPlaylist);
+
+            modelBuilder.Entity<PlaylistTrack>()
+               .HasKey(mc => new { mc.IdPlaylist, mc.IdTrack });
+            modelBuilder.Entity<PlaylistTrack>()
+                .HasOne(mc => mc.Playlist)
+                .WithMany(m => m.PlaylistTrack)
+                .HasForeignKey(mc => mc.IdPlaylist);
+            modelBuilder.Entity<PlaylistTrack>()
+                .HasOne(mc => mc.Track)
+                .WithMany(c => c.PlaylistTrack)
                 .HasForeignKey(mc => mc.IdTrack);
         }
 
