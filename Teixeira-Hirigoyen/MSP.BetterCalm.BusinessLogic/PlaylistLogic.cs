d5b1d58d4a513ca46a1b268cfd9e92a43007c541
Mauro Teixeira <MT211753@fi365.ort.edu.uy>
2021-04-16 11:59:44 -0300

    Package BusinessLogic added

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogic/PlaylistLogic.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MSP.BetterCalm.BusinessLogic
+{
+    public class PlaylistLogic
+    {
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogicInterface/IPlaylistLogic.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MSP.BetterCalm.BusinessLogicInterface
+{
+    public interface IPlaylistLogic
+    {
+    }
+}
