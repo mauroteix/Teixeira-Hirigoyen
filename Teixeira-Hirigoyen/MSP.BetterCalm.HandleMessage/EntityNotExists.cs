1c5a3a045ab6be7c52d765876401a26ae81350ed
Mauro Teixeira <MT211753@fi365.ort.edu.uy>
2021-04-17 12:09:46 -0300

    GetAll Refactor

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.HandleMessage/EntityNotExists.cs
@@ -0,0 +1,20 @@
+﻿using MSP.BetterCalm.HandleMessage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Msp.BetterCalm.HandleMessage
+{
+    public class EntityNotExists : ExceptionError
+    {
+        string message;
+        public EntityNotExists(string error)
+        {
+            message = error;
+        }
+        public override string MessageError()
+        {
+            return message;
+        }
+    }
+}
