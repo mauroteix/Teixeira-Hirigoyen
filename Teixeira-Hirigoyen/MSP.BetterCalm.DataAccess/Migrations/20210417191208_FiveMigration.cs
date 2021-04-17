70ea5f689268272349373fe482163b734f064d67
Mauro Teixeira <MT211753@fi365.ort.edu.uy>
2021-04-17 16:13:10 -0300

    FiveMigration

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/Migrations/20210417191208_FiveMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MSP.BetterCalm.DataAccess.Migrations
+{
+    public partial class FiveMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Hour",
+                table: "Track",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "MinSeconds",
+                table: "Track",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hour",
+                table: "Track");
+
+            migrationBuilder.DropColumn(
+                name: "MinSeconds",
+                table: "Track");
+        }
+    }
+}
