16ed794c10875c60153c7ff434b330335902731f
Mauro Teixeira <MT211753@fi365.ort.edu.uy>
2021-04-17 17:33:11 -0300

    AddPlaylist Refactor

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccesTest/PlaylistRepositoryTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSP.BetterCalm.DataAccessTest
+{
+    [TestClass]
+    public class PlaylistRepositoryTest
+    {
+        List<Playlist> listPlaylist;
+        PlaylistRepository repository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            listPlaylist = new List<Playlist>() {
+                new Playlist()
+                {
+                    Id = 1,
+                    Name = "Cumbia",
+                    Image = "",
+                    Description = "Musica para previa"
+                },
+                new Playlist()
+                {
+                    Id = 2,
+                    Name = "Reggaeton",
+                    Image = "",
+                    Description = "Musica gozada"
+                }
+            };
+        }
+
+        [TestMethod]
+        public void AddPlaylist()
+        {
+            var options = new DbContextOptionsBuilder<BetterCalmContext>()
+            .UseInMemoryDatabase(databaseName: "MSP.BetterCalmDatabase").Options;
+            var context = new BetterCalmContext(options);
+            context.Add(listPlaylist[0]);
+            context.SaveChanges();
+            repository = new PlaylistRepository(context);
+            var initial = repository.GetAll().Count();
+
+            repository.Add(listPlaylist[1]);
+            var final = repository.GetAll().Count();
+            context.Database.EnsureDeleted();
+
+            Assert.AreEqual(initial + 1, final);
+        }
+
+
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/PlaylistRepository.cs
@@ -0,0 +1,45 @@
+﻿using MSP.BetterCalm.DataAccessInterface;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSP.BetterCalm.DataAccess
+{
+    public class PlaylistRepository : IData<Playlist>
+    {
+        readonly BetterCalmContext _context;
+
+        public PlaylistRepository(BetterCalmContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Playlist entity)
+        {
+            _context.Playlist.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Playlist entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Playlist Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Playlist> GetAll()
+        {
+            return _context.Playlist.ToList();
+        }
+
+        public void Update(Playlist entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
