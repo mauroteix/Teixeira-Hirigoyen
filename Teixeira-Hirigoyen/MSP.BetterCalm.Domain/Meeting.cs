8b0249bf59ea81272a0b33f7d3b3b41cde09dd35
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-18 16:15:55 -0300

    RegisterUser Refactor

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.Domain/Meeting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.Domain
+{
+    public class Meeting
+    {
+        public int IdMeeting { get; set; }
+        public User User { get; set; }
+        public int IdUser { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Meeting meeting = (Meeting)obj;
+                return (this.IdMeeting.Equals(meeting.IdMeeting));
+            }
+        }
+
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DomainTest/TestMeeting.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.DomainTest
+{
+    [TestClass]
+    public class TestMeeting
+    {
+        User user;
+        Meeting meeting;
+        [TestInitialize]
+        public void Initialize()
+        {
+            user = new User
+            {
+                Name = "Mauro",
+                Id = 1,
+                Surname = "Teixeira",
+                Birthday = new DateTime(1996, 1, 1),
+                Email = "mauroGil@gmail.com",
+                Cellphone = "099156189",
+
+            };
+            meeting = new Meeting
+            {
+                IdMeeting = 1,
+                User = user,
+                IdUser = user.Id,
+            };
+
+        }
+        [TestMethod]
+        public void RegisterUser()
+        {
+            Assert.AreEqual(user, meeting.User);
+        }
+    }
+}
