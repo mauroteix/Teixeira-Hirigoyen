03be88e5a1732b94ca82deb4144ccc2fe77e5729
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-18 15:31:47 -0300

    RegisterName Refactor

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DomainTest/TestPsychologist.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.DomainTest
+{
+    [TestClass]
+    public class TestPsychologist
+    {
+        Psychologist psychologist;
+        [TestInitialize]
+        public void Initialize()
+        {
+            psychologist = new Psychologist
+            {
+                Name = "Mauro",
+                Id = 1,
+            };
+
+
+        }
+        [TestMethod]
+        public void RegisterName()
+        {
+            Assert.AreEqual("Mauro", psychologist.Name);
+        }
+
+    }
+}
