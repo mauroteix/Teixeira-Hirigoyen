bfcc8c929b6f80069d6f33b4c0b96d7439953813
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-23 16:59:00 -0300

    Create Test

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.Domain/Administrator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.Domain
+{
+    public class Administrator
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DomainTest/TestAdministrator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MSP.BetterCalm.Domain;
+
+namespace MSP.BetterCalm.DomainTest
+{
+    [TestClass]
+    public class TestAdministrator
+    {
+
+    }
+}
