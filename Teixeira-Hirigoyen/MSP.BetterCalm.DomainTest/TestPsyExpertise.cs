a6c714d975c2ae3be7e3dd66d4553dbb00d77ebb
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-24 16:30:53 -0300

    Creation Class/Test

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DomainTest/TestPsyExpertise.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.DomainTest
+{
+    [TestClass]
+    public class TestPsyExpertise
+    {
+    }
+}
