8166fe747990c6b06af4d0153c249715322f83f7
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-25 10:27:02 -0300

    Creation class

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using MSP.BetterCalm.DataAccessInterface;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.HandleMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSP.BetterCalm.DataAccess
+{
+    public class UserRepository : IData<User>
+    {
+        readonly BetterCalmContext _context;
+
+        public UserRepository(BetterCalmContext context)
+        {
+            _context = context;
+        }
+        public void Add(User entity)
+        {
+            throw new NotImplementedException();
+           // _context.User.Add(entity);
+            //_context.SaveChanges();
+        }
+
+        public void Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
