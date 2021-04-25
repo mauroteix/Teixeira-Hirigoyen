2f105f02e2fb3ac21e7e792cb179dffceeeb5760
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-25 10:28:32 -0300

    Creation test

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccesTest/UserRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace MSP.BetterCalm.DataAccessTest
+{
+    [TestClass]
+    public class UserRepositoryTest
+    {
+
+    }
+}
