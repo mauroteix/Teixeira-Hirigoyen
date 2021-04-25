94e20e591f64df9bbdd9f255ebdc3d1122b2f966
mauroteix <MT211753@fi365.ort.edu.uy>
2021-04-25 10:47:10 -0300

    GetOnePlaylist Refactor

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.API/Controllers/PlaylistController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSP.BetterCalm.API.Controllers
+{
+    [Route("api/playlist")]
+    [ApiController]
+    public class PlaylistController : ControllerBase
+    {
+        IPlaylistLogic _playlistLogic;
+        public PlaylistController(IPlaylistLogic playlistLogic)
+        {
+            _playlistLogic = playlistLogic;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                Playlist playlist = _playlistLogic.Get(id);
+                return Ok(playlist);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.APITest/PlaylistControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.API.Controllers;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.APITest
+{
+    [TestClass]
+    public class PlaylistControllerTest
+    {
+        List<Playlist> playlistList;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Playlist playlist = new Playlist()
+            {
+                Id = 1,
+                Name = "Variado 2021",
+                Description = "Nuevos hits",
+                PlaylistCategory = new List<PlaylistCategory>(),
+                PlaylistTrack = new List<PlaylistTrack>()
+            };
+            playlistList = new List<Playlist>();
+            playlistList.Add(playlist);
+        }
+
+        [TestMethod]
+        public void GetOnePlaylist()
+        {
+            var mockPlaylist = new Mock<IPlaylistLogic>(MockBehavior.Strict);
+            mockPlaylist.Setup(res => res.Get(playlistList[0].Id)).Returns(playlistList[0]);
+            PlaylistController controller = new PlaylistController(mockPlaylist.Object);
+            var result = controller.Get(playlistList[0].Id);
+
+            mockPlaylist.VerifyAll();
+            Assert.AreEqual(result.ToString(), new OkObjectResult("").ToString());
+        }
+    }
+}
