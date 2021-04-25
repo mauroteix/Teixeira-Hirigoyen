adf85fd1afba02f885de018ca458651e07095df2
mauroteix <MT211753@fi365.ort.edu.uy>
2021-04-25 10:18:35 -0300

    GetAllPlaylist Refactor

--- Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogicInterface/IPlaylistLogic.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogicInterface/IPlaylistLogic.cs
@@ -1,12 +1,15 @@
 ﻿using MSP.BetterCalm.Domain;
 using System;
+using System.Collections.Generic;
 
 namespace MSP.BetterCalm.BusinessLogicInterface
 {
     public interface IPlaylistLogic
     {
         public Playlist Get(int id);
-   
+        public void Add(Playlist playlist);
+        public List<Playlist> GetAll();
+
         /* 
          AddPlaylist
          GetPlaylistById
