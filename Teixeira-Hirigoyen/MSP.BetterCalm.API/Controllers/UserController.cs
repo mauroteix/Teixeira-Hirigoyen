009690961bab4add8c30b73ba634075d522ef066
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-26 18:41:51 -0300

    Creation UserController

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.API/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Msp.BetterCalm.HandleMessage;
+using MSP.BetterCalm.BusinessLogicInterface;
+using System;
+using System.Linq;
+
+namespace MSP.BetterCalm.API.Controllers
+{
+
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        IUserLogic _userLogic;
+        public UserController(IUserLogic userLogic)
+        {
+            _userLogic = userLogic;
+        }
+    }
+}
