f27b82641d26604491727ac0d369e44470160c19
mauroteix <MT211753@fi365.ort.edu.uy>
2021-04-26 14:45:30 -0300

    GetOneTrack Refactor

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.API/Controllers/TrackController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSP.BetterCalm.API.Controllers
+{
+    [Route("api/track")]
+    [ApiController]
+    public class TrackController : ControllerBase
+    {
+        ITrackLogic _trackLogic;
+        public TrackController(ITrackLogic trackLogic)
+        {
+            _trackLogic = trackLogic;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                Track track = _trackLogic.Get(id);
+                return Ok(track);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+    
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.APITest/TrackControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.API.Controllers;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.APITest
+{
+    [TestClass]
+    public class TrackControllerTest
+    {
+        List<Track> trackList;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Track track = new Track()
+            {
+                Id = 1,
+                Name = "Despacito",
+                Author = "Luis Fonsi",
+                Sound = "despacito-fonsi.mp3",
+                Hour = 0,
+                MinSeconds = 3.2,
+                CategoryTrack = new List<CategoryTrack>(),
+                PlaylistTrack = new List<PlaylistTrack>()
+            };
+            trackList = new List<Track>();
+            trackList.Add(track);
+        }
+
+        [TestMethod]
+        public void GetOneTrack()
+        {
+            var mockPlaylist = new Mock<ITrackLogic>(MockBehavior.Strict);
+            mockPlaylist.Setup(res => res.Get(trackList[0].Id)).Returns(trackList[0]);
+            TrackController controller = new TrackController(mockPlaylist.Object);
+            var result = controller.Get(trackList[0].Id);
+
+            mockPlaylist.VerifyAll();
+            Assert.AreEqual(result.ToString(), new OkObjectResult("").ToString());
+        }
+    }
+}
