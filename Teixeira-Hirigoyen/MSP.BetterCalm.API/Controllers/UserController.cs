7f1036ef2b46de4db26f413746303ff941147874
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-26 19:11:50 -0300

    AddUser Refactor

--- Teixeira-Hirigoyen/MSP.BetterCalm.API/Controllers/UserController.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.API/Controllers/UserController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Msp.BetterCalm.HandleMessage;
 using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.HandleMessage;
 using System;
 using System.Linq;
 
@@ -16,6 +18,23 @@ namespace MSP.BetterCalm.API.Controllers
         public UserController(IUserLogic userLogic)
         {
             _userLogic = userLogic;
+        }
+        [HttpPost()]
+        public IActionResult Add([FromBody] User user)
+        {
+            try
+            {
+                _userLogic.Add(user);
+                return Ok("Successfully added user name:" + user.Name);
+            }
+            catch (FieldEnteredNotCorrect fe)
+            {
+                return UnprocessableEntity(fe.MessageError());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
         }
     }
 }
--- Teixeira-Hirigoyen/MSP.BetterCalm.APITest/UserControllerTest.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.APITest/UserControllerTest.cs
@@ -13,5 +13,36 @@ namespace MSP.BetterCalm.APITest
     [TestClass]
     class UserControllerTest
     {
+        List<User> userlistList;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            User user = new User
+            {
+                Name = "Mauro",
+                Id = 1,
+                Surname = "Teixeira",
+                Birthday = new DateTime(1996, 1, 1),
+                Email = "mauroGil@gmail.com",
+                Cellphone = "099156189",
+
+            };
+            userlistList = new List<User>();
+            userlistList.Add(user);
+        }
+        [TestMethod]
+        public void AddOneUser()
+        {
+            var mockUser = new Mock<IUserLogic>(MockBehavior.Strict);
+            mockUser.Setup(res => res.Add(userlistList[0]));
+            UserController controller = new UserController(mockUser.Object);
+
+            var result = controller.Add(userlistList[0]);
+            var okResult = result as OkObjectResult;
+
+            Assert.AreEqual(new ObjectResult("").ToString(), controller.Add(userlistList[0]).ToString());
+        }
+
     }
 }
