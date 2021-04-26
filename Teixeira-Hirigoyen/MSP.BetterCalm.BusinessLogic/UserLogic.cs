b5decf5c2b248974f09a1caee2d16135b2b922bc
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-26 16:31:12 -0300

    Creation UserLogic/Test

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogic/UserLogic.cs
@@ -0,0 +1,24 @@
+﻿using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.DataAccessInterface;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSP.BetterCalm.BusinessLogic
+{
+    public class UserLogic : IUserLogic
+    {
+        IData<User> _repository;
+        public UserLogic(IData<User> repository)
+        {
+            _repository = repository;
+        }
+        public void Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogicTest/TestUserLogic.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.DataAccessInterface;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.DTO;
+using System.Collections.Generic;
+
+namespace MSP.BetterCalm.BusinessLogicTest
+{
+    [TestClass]
+    public class TestUserLogic
+    {
+
+    }
+}
