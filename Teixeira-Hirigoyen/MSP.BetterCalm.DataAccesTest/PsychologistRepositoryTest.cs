f9049fb455902347e38b06328e44514f03e30e72
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-29 19:04:25 -0300

    Create classs/Test

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccesTest/PsychologistRepositoryTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSP.BetterCalm.DataAccessTest
+{
+    [TestClass]
+    public class PsychologistRepositoryTest
+    {
+
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/PsychologistRepository.cs
@@ -0,0 +1,15 @@
+﻿using MSP.BetterCalm.DataAccessInterface;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.HandleMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+;
+
+namespace MSP.BetterCalm.DataAccess
+{
+    public class PsychologistRepository
+    {
+    }
+}
