a7b986c1f341e86d5cbad813f691be494970f027
mauroteix <MT211753@fi365.ort.edu.uy>
2021-04-30 18:36:21 -0300

    AdministratorController added

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.API/Controllers/AdministratorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSP.BetterCalm.API.Controllers
+{
+    [Route("api/administrator")]
+    [ApiController]
+    public class AdministratorController
+    {
+    }
+}
