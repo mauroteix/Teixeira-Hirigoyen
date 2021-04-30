d7e428700562905d687391aa49d4a9e04cc08c0e
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-04-30 16:48:47 -0300

    Create class/Test

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogic/PsychologistLogic.cs
@@ -0,0 +1,36 @@
+﻿using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.BusinessLogic
+{
+    public class PsychologistLogic : IPsychologistLogic
+    {
+        public void Add(Psychologist psychologist)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Psychologist psychologist)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Psychologist Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Psychologist> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Psychologist psychologist, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogicTest/TestPsychologistLogic.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Msp.BetterCalm.HandleMessage;
+using MSP.BetterCalm.BusinessLogic;
+using MSP.BetterCalm.DataAccessInterface;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.HandleMessage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.BusinessLogicTest
+{
+    [TestClass]
+    public class TestPsychologistLogic
+    {
+
+    }
+}
