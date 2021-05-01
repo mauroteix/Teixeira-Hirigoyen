9a2fb7de97313d48d8fb69c8993d1e1356ad5ee3
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-05-01 17:13:48 -0300

    Create class/test

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.API/Controllers/MedicalConditionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Msp.BetterCalm.HandleMessage;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.HandleMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSP.BetterCalm.API.Controllers
+{
+
+    [Route("api/medicalcondition")]
+    [ApiController]
+    public class MedicalConditionController : ControllerBase
+    {
+        IMedicalConditionLogic _medicalConditionLogic;
+        public MedicalConditionController(IMedicalConditionLogic medicalConditionLogic)
+        {
+            _medicalConditionLogic = medicalConditionLogic;
+        }
+    }
+}
