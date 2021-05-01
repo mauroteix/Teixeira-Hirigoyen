de877f8d9b24f9f34b9af66779133b57be6d192b
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-05-01 15:24:51 -0300

    GetAll Refactor

--- Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogic/MedicalConditionLogic.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogic/MedicalConditionLogic.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using MSP.BetterCalm.DataAccessInterface;
+using MSP.BetterCalm.Domain;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MSP.BetterCalm.BusinessLogic
 {
     public class MedicalConditionLogic
     {
+        IData<MedicalCondition> _repository;
+        public MedicalConditionLogic(IData<MedicalCondition> repository)
+        {
+            _repository = repository;
+        }
+        public List<MedicalCondition> GetAll()
+        {
+            return _repository.GetAll().ToList();
+        }
     }
 }
--- Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogicTest/TestMedicalConditionLogic.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogicTest/TestMedicalConditionLogic.cs
@@ -11,6 +11,38 @@ namespace MSP.BetterCalm.BusinessLogicTest
     [TestClass]
     public class TestMedicalConditionLogic
     {
-
+        MedicalCondition medicalCondition;
+        MedicalCondition secondMedicalCondition;
+        Mock<IData<MedicalCondition>> repository;
+        MedicalConditionLogic medicalConditionLogic;
+        List<MedicalCondition> medicalConditionList;
+        [TestInitialize]
+        public void Initialize()
+        {
+            medicalCondition = new MedicalCondition()
+            {
+                Id = 1,
+                Name = "Dormir"
+            };
+            secondMedicalCondition = new MedicalCondition()
+            {
+                Id = 2,
+                Name = "Musica"
+            };
+            medicalConditionList = new List<MedicalCondition>();
+            medicalConditionList.Add(medicalCondition);
+            medicalConditionList.Add(secondMedicalCondition);
+            repository = new Mock<IData<MedicalCondition>>();
+            repository.Setup(r => r.GetAll()).Returns(medicalConditionList);
+            repository.Setup(play => play.Get(1)).Returns(medicalCondition);
+            repository.Setup(play => play.Add(medicalCondition));
+            medicalConditionLogic = new MedicalConditionLogic(repository.Object);
+        }
+        [TestMethod]
+        public void GetAll()
+        {
+            List<MedicalCondition> list = medicalConditionLogic.GetAll();
+            Assert.AreEqual(list.Count, 2);
+        }
     }
 }
