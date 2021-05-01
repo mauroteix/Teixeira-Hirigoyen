372c5764113dfca09369473b05f95e5f1b92e769
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-05-01 17:48:22 -0300

    Create class/test

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.API/Controllers/PsychologistController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Msp.BetterCalm.HandleMessage;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.HandleMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MSP.BetterCalm.API.Controllers
+{
+    [Route("api/playlist")]
+    [ApiController]
+    public class PsychologistController : ControllerBase
+    {
+        IPsychologistLogic _psychologistLogic;
+        public PsychologistController(IPsychologistLogic psychologistLogic)
+        {
+            _psychologistLogic = psychologistLogic;
+        }
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.APITest/PsychologistControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.API.Controllers;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.HandleMessage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MSP.BetterCalm.APITest
+{
+    [TestClass]
+    public class PsychologistControllerTest
+    {
+
+    }
+}
