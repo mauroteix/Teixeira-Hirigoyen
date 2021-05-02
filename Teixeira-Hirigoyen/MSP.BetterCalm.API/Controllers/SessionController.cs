f17e22110e198141e89005207a830e8997c799f3
mauroteix <MT211753@fi365.ort.edu.uy>
2021-05-02 14:00:25 -0300

    LoginAdminOk Refactor

---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.API/Controllers/SessionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using MSP.BetterCalm.HandleMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSP.BetterCalm.API.Controllers
+{
+    [Route("api/administrator")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+
+        private readonly ISessionLogic sessionLogic;
+
+        public SessionController(ISessionLogic sessionLogic)
+        {
+            this.sessionLogic = sessionLogic;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] Administrator admin)
+        {
+            try
+            {
+                var token = this.sessionLogic.Login(admin);
+                return Ok(token);
+            }
+            catch (FieldEnteredNotCorrect e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+    }
+}
---
+++ Teixeira-Hirigoyen/MSP.BetterCalm.APITest/SessionControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.API.Controllers;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.APITest
+{
+    [TestClass]
+    public class SessionControllerTest
+    {
+
+        [TestMethod]
+        public void LoginAdminOk()
+        {
+            var admin = new Administrator()
+            {
+                Email = "mauro@hotmail.com",
+                Password = "12345"
+            };
+
+            var tokenToReturn = Guid.NewGuid();
+
+            var mock = new Mock<ISessionLogic>(MockBehavior.Strict);
+            mock.Setup(m => m.Login(It.IsAny<Administrator>())).Returns(tokenToReturn);
+            var controller = new SessionController(mock.Object);
+
+            var result = controller.Login(admin);
+            var status = result as OkObjectResult;
+            var content = status.Value.ToString();
+
+            mock.VerifyAll();
+            Assert.AreEqual(content, tokenToReturn.ToString());
+        }
+    }
+}
