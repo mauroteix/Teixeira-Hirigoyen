d96d33725ce94e6d029d9276c46ffdfbc013b3b6
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-05-02 11:15:11 -0300

    Create Meeting

--- Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogic/PsychologistLogic.cs
+++ Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogic/PsychologistLogic.cs
@@ -118,5 +118,83 @@ namespace MSP.BetterCalm.BusinessLogic
 
             _repository.Update(unPsychologist);
         }
+        public User createMeeting(User user)
+        {
+            var medicalCondition = user.MedicalCondition;
+            var date = DateTime.Now;
+            date = changeDate(date);
+            var list = ListOfPsychologist(user.MedicalCondition);
+            var listFreePsy = ListFreePsychologist(list, date);
+            Psychologist psy = new Psychologist();
+            while(listFreePsy.Count == 0)
+            {
+                date = date.AddDays(1);
+                date = changeDate(date);
+                listFreePsy = ListFreePsychologist(list, date);
+            }
+            if(listFreePsy.Count > 1)
+            {
+                psy = SelectOlderPsychologist(listFreePsy);
+            }
+            else
+            {
+                psy = listFreePsy[0];
+            }
+            Meeting meeting = new Meeting();
+            meeting.IdUser = user.Id;
+            meeting.User = user;
+            meeting.Psychologist = psy;
+            meeting.IdPsychologist = psy.Id;
+            user.Meeting.Add(meeting);
+
+            return user;
+        }
+        public DateTime  changeDate(DateTime date)
+        {
+            while (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                date = date.AddDays(1);
+            }
+            return date;
+        }
+        public List<Psychologist> ListOfPsychologist(MedicalCondition medicalCondition)
+        {
+            var list = medicalCondition.Expertise.ToList();
+            List <Psychologist> listPsy = new List<Psychologist>();
+            list.ForEach(c => listPsy.Add(c.Psychologist));
+            return listPsy;
+        }
+        public List<Psychologist> ListFreePsychologist(List<Psychologist> listPsychologist,DateTime date)
+        {
+            var list = new List<Psychologist>();
+            listPsychologist.ForEach(c =>
+            {
+                if (isFreeForMeeting(c,date)) list.Add(c);
+            });
+            return list;
+        }
+        public bool isFreeForMeeting(Psychologist psy,DateTime date) {
+            var list = psy.Meeting.ToList();
+            var listMeetingDate = new List<Meeting>();
+            list.ForEach(c => 
+            {
+                if (c.Date.Equals(date)) listMeetingDate.Add(c);
+            });
+            return listMeetingDate.Count < 6;
+        }
+        public Psychologist SelectOlderPsychologist(List<Psychologist> list)
+        {
+            int position = 0;
+            int minId = list[0].Id;
+            for(int i  = 0; i < list.Count; i++)
+            {
+                if(list[i].Id < minId)
+                {
+                    position = i;
+                    minId = list[i].Id;
+                }
+            }
+            return list[position];
+        }
     }
 }
