f303b1f160984433d40533f4980e9d6028a6e164
mauroteix <MT211753@fi365.ort.edu.uy>
2021-05-21 17:19:51 -0300

    RegisterName Refactor

---
+++ Codigo fuente/Teixeira-Hirigoyen/MSP.BetterCalm.DomainTest/TestVideo.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.DomainTest
+{
+    [TestClass]
+    public class TestVideo
+    {
+        Video video;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            video = new Video
+            {
+                Id = 1,
+                Name = "Bailando"
+            };
+
+        }
+
+        [TestMethod]
+        public void RegisterName()
+        {
+            Assert.AreEqual("Bailando", video.Name);
+        }
+    }
+}
