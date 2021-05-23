6553ddcd3e3d34265f07e62d81b0e84608b12439
mauroteix <MT211753@fi365.ort.edu.uy>
2021-05-23 12:26:12 -0300

    RegisterPlaylistId Refactor

---
+++ Codigo fuente/Teixeira-Hirigoyen/MSP.BetterCalm.Domain/PlaylistVideo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.Domain
+{
+    public class PlaylistVideo
+    {
+        public int IdPlaylist { get; set; }
+        public Playlist Playlist { get; set; }
+
+        public PlaylistVideo()
+        { }
+    }
+}
---
+++ Codigo fuente/Teixeira-Hirigoyen/MSP.BetterCalm.DomainTest/TestPlaylistVideo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.BetterCalm.DomainTest
+{
+    [TestClass]
+    public class TestPlaylistVideo
+    {
+        Playlist playlist;
+        PlaylistVideo playlistVideo;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            playlistVideo = new PlaylistVideo();
+            playlist = new Playlist
+            {
+                Name = "Cumbia",
+                Id = 1,
+                Description = "Boliche",
+                Image = ""
+            };
+        }
+
+        [TestMethod]
+        public void RegisterPlaylistId()
+        {
+            playlistVideo.IdPlaylist = playlist.Id;
+            Assert.AreEqual(playlist.Id, playlistVideo.IdPlaylist);
+        }
+
+    }
+}
