721c19a8f44e6e148a46ccb98d2d5b5d53a1c09f
mauroteix <MT211753@fi365.ort.edu.uy>
2021-05-28 12:00:31 -0300

    AddVideo  Refactor

---
+++ Codigo fuente/Teixeira-Hirigoyen/MSP.BetterCalm.DataAccesTest/VideoRepositoryTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSP.BetterCalm.DataAccess;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSP.BetterCalm.DataAccessTest
+{
+    [TestClass]
+    public class VideoRepositoryTest
+    {
+        List<Video> listVideo;
+        VideoRepository repository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            listVideo = new List<Video>() {
+                new Video()
+                {
+                    Id = 1,
+                    Name = "Bailando",
+                    Author = "Rodrigo",
+                    Hour = 0,
+                    MinSeconds = 1.25,
+                    LinkVideo = "www.youtube.com/bailandorodrigo"
+                },
+                new Video()
+                {
+                    Id = 2,
+                    Name = "Gasolina",
+                    Author = "Mauro",
+                    Hour = 2,
+                    MinSeconds = 30.1,
+                    LinkVideo = "www.youtube.com/gasolinamauro"
+
+                }
+            };
+        }
+
+        [TestMethod]
+        public void AddVideo()
+        {
+            var options = new DbContextOptionsBuilder<BetterCalmContext>()
+            .UseInMemoryDatabase(databaseName: "MSP.BetterCalmDatabase").Options;
+            var context = new BetterCalmContext(options);
+            context.Add(listVideo[0]);
+            context.SaveChanges();
+            repository = new VideoRepository(context);
+            var initial = repository.GetAll().Count();
+
+            repository.Add(listVideo[1]);
+            var final = repository.GetAll().Count();
+            context.Database.EnsureDeleted();
+
+            Assert.AreEqual(initial + 1, final);
+        }
+    }
+}
---
+++ Codigo fuente/Teixeira-Hirigoyen/MSP.BetterCalm.DataAccess/VideoRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MSP.BetterCalm.DataAccessInterface;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSP.BetterCalm.DataAccess
+{
+    public class VideoRepository : IData<Video>
+    {
+        readonly BetterCalmContext _context;
+
+        public VideoRepository(BetterCalmContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Video entity)
+        {
+            _context.Video.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Video entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Video> GetAll()
+        {
+            return _context.Video
+                .Include(t => t.CategoryVideo).ThenInclude(u => u.Category)
+                .Include(r => r.PlaylistVideo).ThenInclude(s => s.Playlist)
+                .ToList();
+        }
+
+        public void Update(Video entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
