31f51871e609f839c7fac76681a2579d6f734b2a
mauroteix <MT211753@fi365.ort.edu.uy>
2021-06-01 12:11:53 -0300

    GetOneVideo Refactor

---
+++ Codigo fuente/Teixeira-Hirigoyen/MSP.BetterCalm.API/Controllers/VideoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Msp.BetterCalm.HandleMessage;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSP.BetterCalm.API.Controllers
+{
+    public class VideoController : ControllerBase
+    {
+
+        IVideoLogic _videoLogic;
+        public VideoController(IVideoLogic videoLogic)
+        {
+            _videoLogic = videoLogic;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                Video video = _videoLogic.Get(id);
+                return Ok(video);
+            }
+            catch (EntityNotExists fe)
+            {
+                return NotFound(fe.MessageError());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+    }
+}
---
+++ Codigo fuente/Teixeira-Hirigoyen/MSP.BetterCalm.APITest/VideoControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MSP.BetterCalm.API.Controllers;
+using MSP.BetterCalm.BusinessLogicInterface;
+using MSP.BetterCalm.Domain;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace MSP.BetterCalm.APITest
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class VideoControllerTest
+    {
+        List<Video> videoList;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Category category = new Category
+            {
+                Name = "Musica",
+                Id = 2
+            };
+            Video video = new Video()
+            {
+                Id = 1,
+                Name = "Despacito",
+                Author = "Luis Fonsi",
+                LinkVideo = "despacito-fonsi.com",
+                Hour = 0,
+                MinSeconds = 3.2,
+                CategoryVideo = new List<CategoryVideo>(),
+                PlaylistVideo = new List<PlaylistVideo>()
+            };
+            CategoryVideo categoryVideo = new CategoryVideo
+            {
+                IdCategory = category.Id
+            };
+            video.CategoryVideo.Add(categoryVideo);
+            videoList = new List<Video>();
+            videoList.Add(video);
+        }
+
+        [TestMethod]
+        public void GetOneVideo()
+        {
+            var mockVideo = new Mock<IVideoLogic>(MockBehavior.Strict);
+            mockVideo.Setup(res => res.Get(videoList[0].Id)).Returns(videoList[0]);
+            VideoController controller = new VideoController(mockVideo.Object);
+            var result = controller.Get(videoList[0].Id);
+
+            mockVideo.VerifyAll();
+            Assert.AreEqual(result.ToString(), new OkObjectResult("").ToString());
+        }
+    }
+}
