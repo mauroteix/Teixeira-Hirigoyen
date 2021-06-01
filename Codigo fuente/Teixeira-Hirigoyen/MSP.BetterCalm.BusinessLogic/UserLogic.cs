f2bc4704cf66c91c7c8a3c2370df0196044b21a0
Rodrigo Hirigoyen <rohigon@hotmail.com>
2021-06-01 12:29:41 -0300

    CalculateDiscount Refactor

--- Codigo fuente/Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogic/UserLogic.cs
+++ Codigo fuente/Teixeira-Hirigoyen/MSP.BetterCalm.BusinessLogic/UserLogic.cs
@@ -174,15 +174,19 @@ namespace MSP.BetterCalm.BusinessLogic
         private double CalculateDiscount(double price, int discounttoapply)
         {
             double realprice = 0;
-            if(discounttoapply == 1)
+            if (discounttoapply == 1)
             {
-                realprice = price*0.85;
+                realprice = price;
             }
             if (discounttoapply == 2)
             {
-                realprice = price * 0.75;
+                realprice = price*0.85;
             }
             if (discounttoapply == 3)
+            {
+                realprice = price * 0.75;
+            }
+            if (discounttoapply == 4)
             {
                 realprice = price * 0.50;
             }
